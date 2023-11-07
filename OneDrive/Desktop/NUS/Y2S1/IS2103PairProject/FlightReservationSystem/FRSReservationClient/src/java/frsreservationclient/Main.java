bb34b722271d881b18a22c189699ae256bdf4e22
Alvin Tor <alvintjw@Alvins-MacBook-Air.local>
2023-11-07 11:06:02 +0800

    changed main class for reservation

--- OneDrive/Desktop/NUS/Y2S1/IS2103PairProject/FlightReservationSystem/FRSReservationClient/src/java/frsreservationclient/Main.java
+++ OneDrive/Desktop/NUS/Y2S1/IS2103PairProject/FlightReservationSystem/FRSReservationClient/src/java/frsreservationclient/Main.java
@@ -14,6 +14,7 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("hello");
         // TODO code application logic here
     }
     
