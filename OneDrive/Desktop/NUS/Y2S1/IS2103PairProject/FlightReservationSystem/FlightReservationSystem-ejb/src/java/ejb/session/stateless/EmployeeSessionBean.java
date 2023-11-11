c56ce1f3874cf330a587996425bcbaf20a0fa188
zuyuan-alexander <e0985949@u.nus.edu>
2023-11-11 22:00:02 +0800

    Added EmployeeSessionBean by zuyuan

---
+++ OneDrive/Desktop/NUS/Y2S1/IS2103PairProject/FlightReservationSystem/FlightReservationSystem-ejb/src/java/ejb/session/stateless/EmployeeSessionBean.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/J2EE/EJB30/StatelessEjbClass.java to edit this template
+ */
+package ejb.session.stateless;
+
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author zuyua
+ */
+@Stateless
+public class EmployeeSessionBean implements EmployeeSessionBeanRemote, EmployeeSessionBeanLocal {
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+}
