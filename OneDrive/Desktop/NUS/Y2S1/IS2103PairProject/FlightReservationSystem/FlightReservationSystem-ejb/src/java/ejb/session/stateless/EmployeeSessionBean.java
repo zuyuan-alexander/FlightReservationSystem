b9d9b57ad4c2631fa21c2f4d51bee65d0b1c7a04
Alvin Tor <alvintjw@Alvins-MacBook-Air.local>
2023-11-12 21:07:28 +0800

    EmployeeSessionBean

--- OneDrive/Desktop/NUS/Y2S1/IS2103PairProject/FlightReservationSystem/FlightReservationSystem-ejb/src/java/ejb/session/stateless/EmployeeSessionBean.java
+++ OneDrive/Desktop/NUS/Y2S1/IS2103PairProject/FlightReservationSystem/FlightReservationSystem-ejb/src/java/ejb/session/stateless/EmployeeSessionBean.java
@@ -4,15 +4,137 @@
  */
 package ejb.session.stateless;
 
+import entity.Employee;
+import java.util.Set;
 import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.NonUniqueResultException;
+import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceException;
+import javax.persistence.Query;
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+import util.exception.EmployeeNotFoundException;
+import util.exception.EmployeeUsernameExistException;
+import util.exception.InputDataValidationException;
+import util.exception.InvalidLoginCredentialException;
+import util.exception.UnknownPersistenceException;
 
 /**
  *
- * @author zuyua
+ * @author alvintjw
  */
 @Stateless
 public class EmployeeSessionBean implements EmployeeSessionBeanRemote, EmployeeSessionBeanLocal {
 
+    @PersistenceContext(unitName = "FlightReservationSystem-ejbPU")
+    private EntityManager em;
+    
+    private final ValidatorFactory validatorFactory;
+    private final Validator validator;
+    
+    
+    
+    public EmployeeSessionBean()
+    {
+        validatorFactory = Validation.buildDefaultValidatorFactory();
+        validator = validatorFactory.getValidator();
+    }
+    
+    @Override
+    public Long createNewEmployee(Employee newEmployee) throws EmployeeUsernameExistException, UnknownPersistenceException, InputDataValidationException
+    {
+        Set<ConstraintViolation<Employee>>constraintViolations = validator.validate(newEmployee);
+        
+        if(constraintViolations.isEmpty())
+        {
+            try
+            {
+                em.persist(newEmployee);
+                em.flush();
+
+                return newEmployee.getEmployeeid();
+            }
+            catch(PersistenceException ex)
+            {
+                if(ex.getCause() != null && ex.getCause().getClass().getName().equals("org.eclipse.persistence.exceptions.DatabaseException"))
+                {
+                    if(ex.getCause().getCause() != null && ex.getCause().getCause().getClass().getName().equals("java.sql.SQLIntegrityConstraintViolationException"))
+                    {
+                        throw new EmployeeUsernameExistException();
+                    }
+                    else
+                    {
+                        throw new UnknownPersistenceException(ex.getMessage());
+                    }
+                }
+                else
+                {
+                    throw new UnknownPersistenceException(ex.getMessage());
+                }
+            }
+        }
+        else
+        {
+            throw new InputDataValidationException(prepareInputDataValidationErrorsMessage(constraintViolations));
+        }
+    }
+
+    @Override
+    public Employee employeeLogin(String username, String password) throws InvalidLoginCredentialException
+    {
+        try
+        {
+            Employee employee = retrieveEmployeeByUsername(username);
+            
+            if(employee.getPassword().equals(password))
+            {
+                //employee.getSaleTransactionEntities().size();                
+                return employee;
+            }
+            else
+            {
+                throw new InvalidLoginCredentialException("Username does not exist or invalid password!");
+            }
+        }
+        catch(EmployeeNotFoundException ex)
+        {
+            throw new InvalidLoginCredentialException("Username does not exist or invalid password!");
+        }
+    }
+    
+    @Override
+    public Employee retrieveEmployeeByUsername(String username) throws EmployeeNotFoundException
+    {
+        Query query = em.createQuery("SELECT e FROM Employee e WHERE e.username = :inUsername");
+        query.setParameter("inUsername", username);
+        
+        try
+        {
+            return (Employee)query.getSingleResult();
+        }
+        catch(NoResultException | NonUniqueResultException ex)
+        {
+            throw new EmployeeNotFoundException("Employee Username " + username + " does not exist!");
+        }
+    }
+    
+    private String prepareInputDataValidationErrorsMessage(Set<ConstraintViolation<Employee>>constraintViolations)
+    {
+        String msg = "Input data validation error!:";
+            
+        for(ConstraintViolation constraintViolation:constraintViolations)
+        {
+            msg += "\n\t" + constraintViolation.getPropertyPath() + " - " + constraintViolation.getInvalidValue() + "; " + constraintViolation.getMessage();
+        }
+        
+        return msg;
+    }
+    
+    
     // Add business logic below. (Right-click in editor and choose
     // "Insert Code > Add Business Method")
 }
--- OneDrive/Desktop/NUS/Y2S1/IS2103PairProject/FlightReservationSystem/FlightReservationSystem-ejb/src/java/ejb/session/stateless/EmployeeSessionBeanLocal.java
+++ OneDrive/Desktop/NUS/Y2S1/IS2103PairProject/FlightReservationSystem/FlightReservationSystem-ejb/src/java/ejb/session/stateless/EmployeeSessionBeanLocal.java
@@ -4,13 +4,22 @@
  */
 package ejb.session.stateless;
 
+import entity.Employee;
 import javax.ejb.Local;
+import util.exception.EmployeeNotFoundException;
+import util.exception.EmployeeUsernameExistException;
+import util.exception.InputDataValidationException;
+import util.exception.InvalidLoginCredentialException;
+import util.exception.UnknownPersistenceException;
 
 /**
  *
- * @author zuyua
+ * @author alvintjw
  */
 @Local
 public interface EmployeeSessionBeanLocal {
-    
+    public Employee retrieveEmployeeByUsername(String username) throws EmployeeNotFoundException;
+    public Employee employeeLogin(String username, String password) throws InvalidLoginCredentialException;
+    public Long createNewEmployee(Employee newEmployee) throws EmployeeUsernameExistException, UnknownPersistenceException, InputDataValidationException;
+
 }
