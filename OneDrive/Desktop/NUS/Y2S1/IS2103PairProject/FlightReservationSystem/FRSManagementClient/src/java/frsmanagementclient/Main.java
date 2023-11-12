8edd92df8b5c9133378c3f0d5eddb0cff70d3f57
Alvin Tor <alvintjw@Alvins-MacBook-Air.local>
2023-11-12 22:03:35 +0800

    Main and MainApp

--- OneDrive/Desktop/NUS/Y2S1/IS2103PairProject/FlightReservationSystem/FRSManagementClient/src/java/frsmanagementclient/Main.java
+++ OneDrive/Desktop/NUS/Y2S1/IS2103PairProject/FlightReservationSystem/FRSManagementClient/src/java/frsmanagementclient/Main.java
@@ -4,18 +4,25 @@
  */
 package frsmanagementclient;
 
+import ejb.session.stateless.EmployeeSessionBeanRemote;
+import javax.ejb.EJB;
+
 /**
  *
  * @author zuyua
  */
 public class Main {
 
+    @EJB
+    private static EmployeeSessionBeanRemote employeeSessionBean;
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Testing");
+        MainApp mainApp = new MainApp(employeeSessionBean);
+        mainApp.runApp();
     }
     
 }
---
+++ OneDrive/Desktop/NUS/Y2S1/IS2103PairProject/FlightReservationSystem/FRSManagementClient/src/java/frsmanagementclient/MainApp.java
@@ -0,0 +1,99 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package frsmanagementclient;
+
+import ejb.session.stateless.EmployeeSessionBeanRemote;
+import entity.Employee;
+import static java.lang.Math.E;
+import java.util.Scanner;
+import util.exception.InvalidLoginCredentialException;
+
+/**
+ *
+ * @author alvintjw
+ */
+public class MainApp {
+    
+    private EmployeeSessionBeanRemote employeeSessionBean;
+    private Employee currentEmployee;
+
+    public MainApp(EmployeeSessionBeanRemote employeeSessionBean) {
+        this.employeeSessionBean = employeeSessionBean;
+    }
+      
+    public void runApp()
+    {
+        Scanner scanner = new Scanner(System.in);
+        Integer response = 0;
+        
+        while(true)
+        {
+            System.out.println("*** Welcome to FRS Mangagement System***\n");
+            System.out.println("1: Login");
+            System.out.println("2: Exit\n");
+            response = 0;
+            
+            while(response < 1 || response > 2)
+            {
+                System.out.print("> ");
+
+                response = scanner.nextInt();
+
+                if(response == 1)
+                {
+                    try
+                    {
+                        doLogin();
+                        System.out.println("Login successful!\n");
+                        
+                        
+                      
+                    }
+                    catch(InvalidLoginCredentialException ex) 
+                    {
+                        System.out.println("Invalid login credential: " + ex.getMessage() + "\n");
+                    }
+                }
+                else if (response == 2)
+                {
+                    break;
+                }
+                else
+                {
+                    System.out.println("Invalid option, please try again!\n");                
+                }
+            }
+            
+            if(response == 2)
+            {
+                break;
+            }
+        }
+    }
+     
+     private void doLogin() throws InvalidLoginCredentialException
+    {
+        Scanner scanner = new Scanner(System.in);
+        String username = "";
+        String password = "";
+        
+        System.out.println("*** FRS Management System :: Login ***\n");
+        System.out.print("Enter username> ");
+        username = scanner.nextLine().trim();
+        System.out.print("Enter password> ");
+        password = scanner.nextLine().trim();
+        
+        if(username.length() > 0 && password.length() > 0)
+        {
+            //currentEmployee = employeeSessionBean.employeeLogin(username, password);      
+        }
+        else
+        {
+            throw new InvalidLoginCredentialException("Missing login credential!");
+        }
+    }
+     
+     
+}
