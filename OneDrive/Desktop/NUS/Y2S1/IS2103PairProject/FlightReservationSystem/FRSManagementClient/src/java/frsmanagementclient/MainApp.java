d2c4ae30d864357dddf33a16d167cedef540aebd
Alvin Tor <alvintjw@Alvins-MacBook-Air.local>
2023-11-13 11:00:21 +0800

    MainApp

--- OneDrive/Desktop/NUS/Y2S1/IS2103PairProject/FlightReservationSystem/FRSManagementClient/src/java/frsmanagementclient/MainApp.java
+++ OneDrive/Desktop/NUS/Y2S1/IS2103PairProject/FlightReservationSystem/FRSManagementClient/src/java/frsmanagementclient/MainApp.java
@@ -8,6 +8,7 @@ import ejb.session.stateless.EmployeeSessionBeanRemote;
 import entity.Employee;
 import static java.lang.Math.E;
 import java.util.Scanner;
+import util.enumeration.EmployeeTypeEnum;
 import util.exception.InvalidLoginCredentialException;
 
 /**
@@ -87,11 +88,74 @@ public class MainApp {
         
         if(username.length() > 0 && password.length() > 0)
         {
-            currentEmployee = employeeSessionBean.employeeLogin(username, password);      
+            currentEmployee = employeeSessionBean.employeeLogin(username, password);   
+            if(currentEmployee.getUserRole().equals(EmployeeTypeEnum.SCHEDULE_MANAGER))
+            {
+                FlightOperationModuleMenuMain();
+            }
         }
         else
         {
             throw new InvalidLoginCredentialException("Missing login credential!");
+        }
+    }
+     
+    private void FlightOperationModuleMenuMain()
+    {
+        Scanner scanner = new Scanner(System.in);
+        Integer response = 0;
+        
+        while(true)
+        {
+            System.out.println("*** Welcome to FRS Flight Operation Module***\n");
+            System.out.println("1: Create Flight");
+            System.out.println("2: View All Flights");
+            System.out.println("3: View Flight Details");
+            System.out.println("4: Update Flight");
+            System.out.println("5: Delete Flight");
+            System.out.println("6: Create Flight Schedule Plan");
+            System.out.println("7: View all Flight Schedule Plan");
+            System.out.println("8: View Flight Schedule Plan Details");
+            System.out.println("9: Update Flight Schedule Plan");
+            System.out.println("10: Delete Flight Schedule Plan");
+            System.out.println("11: Exit\n");
+            response = 0;
+            
+            while(response < 1 || response > 11)
+            {
+                System.out.print("> ");
+
+                response = scanner.nextInt();
+
+                if(response == 1)
+                {
+                    try
+                    {
+                        doLogin();
+                        System.out.println("Login successful!\n");
+                        
+                        
+                      
+                    }
+                    catch(InvalidLoginCredentialException ex) 
+                    {
+                        System.out.println("Invalid login credential: " + ex.getMessage() + "\n");
+                    }
+                }
+                else if (response == 11)
+                {
+                    break;
+                }
+                else
+                {
+                    System.out.println("Invalid option, please try again!\n");                
+                }
+            }
+            
+            if(response == 11)
+            {
+                break;
+            }
         }
     }
      
